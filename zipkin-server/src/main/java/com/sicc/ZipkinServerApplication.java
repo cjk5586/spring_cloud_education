b01f7014670dbc3a1bb972790eba7013e61a92a7
sicc-msa-grp <lee.sun.woong@sicc.co.kr>
2019-12-13 16:44:16 +0900

    zipkin server

---
+++ zipkin-server/src/main/java/com/sicc/ZipkinServerApplication.java
@@ -0,0 +1,21 @@
+package com.sicc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import zipkin2.server.internal.EnableZipkinServer;
+import zipkin2.server.internal.RegisterZipkinHealthIndicators;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinServerApplication {
+	static {
+		System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
+	}
+	
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ZipkinServerApplication.class)
+        .listeners(new RegisterZipkinHealthIndicators())
+        .properties("spring.config.name=zipkin-server").run(args);
+	}
+}
