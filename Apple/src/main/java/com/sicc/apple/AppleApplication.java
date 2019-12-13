c9c740bcb80a38249574c338ffa1bf924a170d17
sicc-msa-grp <lee.sun.woong@sicc.co.kr>
2019-12-13 17:52:17 +0900

    config server

---
+++ Apple/src/main/java/com/sicc/apple/AppleApplication.java
@@ -0,0 +1,27 @@
+package com.sicc.apple;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/apple")
+@RefreshScope
+public class AppleApplication {
+	@Value("${test.message}")
+	public String msg;
+	
+	@RequestMapping("/index")
+	public String index() {
+		return msg;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleApplication.class, args);
+	}
+
+}
---
+++ Banana/src/main/java/com/sicc/banana/BananaApplication.java
@@ -0,0 +1,26 @@
+package com.sicc.banana;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/banana")
+@RefreshScope
+public class BananaApplication {
+	@Value("${test.message}")
+	public String msg;
+	
+	@RequestMapping("/index")
+	public String index() {
+		return msg;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananaApplication.class, args);
+	}
+}
