902349fd9ddb66e700fc877d1cb483dccb462537
sicc-msa-grp <lee.sun.woong@sicc.co.kr>
2020-01-08 17:03:06 +0900

    jenkins test

--- Member/src/test/java/com/sicc/member/MemberApplicationTests.java
+++ Member/src/test/java/com/sicc/member/MemberApplicationTests.java
@@ -1,13 +1,14 @@
 package com.sicc.member;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class MemberApplicationTests {
-
+public class MemberApplicationTests {
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
-
 }
--- Work/src/test/java/com/sicc/work/WorkApplicationTests.java
+++ Work/src/test/java/com/sicc/work/WorkApplicationTests.java
@@ -1,13 +1,16 @@
 package com.sicc.work;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class WorkApplicationTests {
+public class WorkApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
